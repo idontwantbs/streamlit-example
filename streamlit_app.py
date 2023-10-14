195515cb40dde4f532908efccf3f141eb5870957
idontwantbs <krisa9176@gmail.com>
2023-10-15 00:06:31 +0530

    fix issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,10 @@
 from collections import namedtuple
-import altair as alt
-import math
-import os
-import pandas as pd
 import streamlit as st
 from langchain.llms import openai
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import chroma
 from langchain.chains import RetrievalQA
-
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
 st.set_page_config(page_title="Ask the assistant")
 st.title("I want an assistant")
