8a4fb67c787d0737cf7680c466f26e6d19edf3df
Ayush Jain <aj4ayushjain@gmail.com>
2023-10-14 17:06:28 +0530

    initial version v1
    Signed-off-by: Ayush Jain <aj4ayushjain@gmail.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,15 @@
 from collections import namedtuple
 import altair as alt
 import math
+import os
 import pandas as pd
 import streamlit as st
+from langchain.llms import openai
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import chroma
+from langchain.chains import RetrievalQA
+
 
 """
 # Welcome to Streamlit!
@@ -15,24 +22,47 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+st.set_page_config(page_title="Ask the assistant")
+st.title("I want an assistant")
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def generate_response(uploaded_file, openai_api_key, query_text):
+    """
+        response from the uploaded file
+    """
+    if uploaded_file is not None:
+        documents = [uploaded_file.read().decode()]
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    # Split documents into text chunks
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.create_documents(documents)
 
-    points_per_turn = total_points / num_turns
+    # Select embeddings
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    db = chroma.from_documents(texts, embeddings)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    retriever = db.as_retriever()
+
+    qa = RetrievalQA.from_chain_type(llm=openai(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+
+    return qa.run(query_text)
+
+
+# File upload
+uploaded_file = st.file_uploader('Upload an article', type=['txt', "pdf"])
+# Query text
+query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
+
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+
+result = []
+
+with st.form('myform', clear_on_submit=True):
+    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
+    if submitted and openai_api_key.startswith('sk-'):
+        with st.spinner('Calculating...'):
+            response = generate_response(uploaded_file, openai_api_key, query_text)
+            result.append(response)
+
+if len(result):
+    st.info(response)
